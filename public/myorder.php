755b6b1e46ad87bee1ac3f02c915bad9f246267d
sakulb <lukas.budos@gmail.com>
2024-09-23 08:54:42 +0200

    Add documentation.

--- public/myorder.php
+++ public/myorder.php
@@ -1,21 +1,11 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
 
-$app = new \Isklad\MyorderCartWidgetMiddleware\IskladApp(
-    new \Isklad\MyorderCartWidgetMiddleware\IskladEnv(
-        '01914a9a-c7ea-7e83-8a3f-dedfca0d391f',
-        '01914a9a-c7ea-7e83-8a3f-dedfca0d391f',
-        34,
-        __DIR__ . '/../data',
-        'https://myorder.local/widget/cart/shop/',
-        'https://auth.local/auth/access-token',
-        'https://auth.local/api/client/device-identity-request',
-        'https://myorder.local',
-        '_isklad_deviceId',
-        '_isklad_deviceIdentityRequestId',
-        '_isklad_csrf_token',
-        true,
-    )
+use Isklad\MyorderCartWidgetMiddleware\IskladApp;
+use Isklad\MyorderCartWidgetMiddleware\IskladEnv;
+
+$app = new IskladApp(
+    IskladEnv::fromIniFile(__DIR__ . '/../env.ini')
 );
 
 $app->myorderApiController();
