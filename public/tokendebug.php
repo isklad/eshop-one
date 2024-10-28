d3d240acad1b03260230d59ecb4ecc75d2df3982
sakulb <lukas.budos@gmail.com>
2024-10-28 19:42:55 +0100

    debug token.

--- public/tokendebug.php
+++ public/tokendebug.php
@@ -11,4 +11,18 @@ error_reporting(E_ALL);
 $env = IskladEnv::fromIniFile(__DIR__ . '/../env.ini');
 $clientTokenStorage = new ClientTokenStorage($env);
 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<pre>
+<?php
 var_dump($clientTokenStorage->getSavedTokenDto());
+?>
+</pre>
+</body>
+</html>
