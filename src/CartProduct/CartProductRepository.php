95873bfd64be342a5e06f9845e91adfd6637413e
sakulb <lukas.budos@gmail.com>
2025-12-03 15:27:25 +0100

    T7535479: Change example products.

--- src/CartProduct/CartProductRepository.php
+++ src/CartProduct/CartProductRepository.php
@@ -9,22 +9,40 @@ final class CartProductRepository
     {
         $product = new CartProduct();
         $product->id = $id;
+        $product->quantity = 1;
 
         switch ($id) {
             case '1':
-                $product->name = 'Apple';
-                $product->description = 'iPhone 16 Pro';
-                $product->price = 1199;
-                $product->imageUrl = 'https://eshop-one-test.isklad.eu/images/iphone_1.webp';
+                $product->name = 'Dior';
+                $product->description = 'Eau De Parfum 100ml';
+                $product->price = 120;
+                $product->imageUrl = 'https://eshop-one-test.isklad.eu/images/widget-img-1.jpg';
                 $product->quantity = 2;
                 break;
             case '2':
-                $product->name = 'Apple';
-                $product->description = 'Macbook Air';
-                $product->price = 999;
-                $product->imageUrl = 'https://eshop-one-test.isklad.eu/images/macbook_air_1.webp';
+                $product->name = 'New Balance';
+                $product->description = 'New Balance 951';
+                $product->price = 120.50;
+                $product->imageUrl = 'https://eshop-one-test.isklad.eu/images/widget-img-2.jpg';
                 $product->leadTime = 8;
-                $product->quantity = 1;
+                break;
+            case '3':
+                $product->name = 'Versace Eros';
+                $product->description = 'Eau De Parfum 100ml';
+                $product->price = 81;
+                $product->imageUrl = 'https://eshop-one-test.isklad.eu/images/widget-img-3.jpg';
+                break;
+            case '4':
+                $product->name = 'Tom Ford Cherry';
+                $product->description = 'Eau De Parfum 100ml';
+                $product->price = 81;
+                $product->imageUrl = 'https://eshop-one-test.isklad.eu/images/widget-img-4.jpg';
+                break;
+            case '5':
+                $product->name = 'Creed Aventus';
+                $product->description = 'Eau De Parfum 100ml';
+                $product->price = 120;
+                $product->imageUrl = 'https://eshop-one-test.isklad.eu/images/widget-img-5.jpg';
                 break;
         }
 
@@ -36,6 +54,9 @@ final class CartProductRepository
         return [
             self::getProductById('1'),
             self::getProductById('2'),
+            self::getProductById('3'),
+            self::getProductById('4'),
+            self::getProductById('5'),
         ];
     }
 }
