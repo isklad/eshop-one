661cdafb46e269adf2cad4ce262d49aa234945e1
sakulb <lukas.budos@gmail.com>
2026-06-24 18:41:48 +0200

    T65465600: Update middleware and Cart.

--- src/Repository/CartRepository.php
+++ src/Repository/CartRepository.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace App\Repository;
 
 use Isklad\MyorderCartWidgetMiddleware\Model\Cart;
+use Isklad\MyorderCartWidgetMiddleware\Model\CartUser;
 
 final class CartRepository
 {
@@ -13,8 +14,12 @@ final class CartRepository
         $cart->orderExternalId = 'ext-' . time();
         $cart->currency = 'EUR';
         $cart->products = ProductRepository::getProducts();
-        $cart->address = AddressRepository::getAddress();
-        $cart->person = PersonRepository::getPerson();
+
+        $buyer = new CartUser();
+        $buyer->address = AddressRepository::getAddress();
+        $buyer->person = PersonRepository::getPerson();
+
+        $cart->buyer = $buyer;
 
         return $cart;
     }
